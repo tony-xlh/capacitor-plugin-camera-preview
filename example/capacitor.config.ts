8570dedf219fbabcdf08baf2091192415ea4b42a
xulihang <xulihanghai@163.com>
2022-10-24 17:51:26 +0800

    example: init capacitor

---
+++ example/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dynamsoft.camerapreview',
+  appName: 'camera-preview',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
