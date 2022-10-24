bb7735c503434c4effdc76c5e30449b6aeb8803b
xulihang <xulihanghai@163.com>
2022-10-24 15:47:54 +0800

    basic web implementation

--- example/src/scripts/index.js
+++ example/src/scripts/index.js
@@ -1,3 +1,25 @@
 import '../styles/index.scss';
+import { CameraPreview } from "capacitor-plugin-camera-preview";
 
 console.log('webpack starterkit');
+
+//let cameraSelect = document.getElementById("cameraSelect");
+//let resolutionSelect = document.getElementById("resolutionSelect");
+//let closeButton = document.getElementById("closeButton");
+//let zoominButton = document.getElementById("zoominButton");
+//let zoomoutButton = document.getElementById("zoomoutButton");
+let startBtn =  document.getElementById("startBtn");
+startBtn.addEventListener("click",startCamera);
+
+initialize();
+
+async function initialize(){
+  await CameraPreview.initialize();
+  startBtn.disabled = "";
+}
+
+async function startCamera(){
+  await CameraPreview.startCamera();
+  startBtn.style.display = "none";
+  document.getElementsByClassName("controls")[0].style.display = "";
+}
